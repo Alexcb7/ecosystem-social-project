ddbaeedc88c211ae081e12f3d4abeb6b07c8f1a8
Alexcb7 <alex.cortell.ifp@gmail.com>
2025-10-21 13:16:22 +0200

    section 2 (responsive)

--- src/components/section2.tsx
+++ src/components/section2.tsx
@@ -15,17 +15,14 @@ function Section2() {
         </Tabs.List>
 
         <Tabs.Content className="tabs-content-bosque" value="bosques">
-          <h1 className='tittle-content-bosque'> Bosques </h1>
           <p className='info-content-bosque'>Los bosques son ecosistemas fundamentales para la vida en el planeta. Albergan una enorme diversidad de flora y fauna, regulan el clima, purifican el aire y protegen el suelo de la erosión. Además, actúan como reservas de agua y carbono, ayudando a mitigar el cambio climático. Los bosques tropicales, templados y boreales ofrecen hábitats únicos y recursos naturales esenciales para millones de especies y comunidades humanas en todo el mundo.</p>
           <img className='image-forest1' src="/images/forest1.jpg" alt="" />
         </Tabs.Content>
         <Tabs.Content className="tabs-content-oceano" value="oceanos">
-          <h1 className='tittle-content-oceano'> Océanos </h1>
           <p className='info-content-oceano'>Los océanos son los ecosistemas más extensos del planeta, cubriendo más del 70 % de su superficie. Albergan una enorme diversidad de vida marina, regulan el clima global y producen gran parte del oxígeno que respiramos. Además, actúan como sumideros de carbono y fuente esencial de alimento y recursos para millones de personas, siendo fundamentales para el equilibrio ecológico y la salud del planeta.</p>
           <img className='image-oceano1' src="/images/oceano1.jpg" alt="" />
         </Tabs.Content>
         <Tabs.Content className="tabs-content-montana" value="montanas">
-          <h1 className='tittle-content-montana'> Montañas </h1>
           <p className='info-content-montana'>Los bosques son ecosistemas fundamentales para la vida en el planeta. Albergan una enorme diversidad de flora y fauna, regulan el clima, purifican el aire y protegen el suelo de la erosión. Además, actúan como reservas de agua y carbono, ayudando a mitigar el cambio climático. Los bosques tropicales, templados y boreales ofrecen hábitats únicos y recursos naturales esenciales para millones de especies y comunidades humanas en todo el mundo.</p>
           <img className='image-montana1' src="/images/montañas1.jpg" alt="" />
         </Tabs.Content>
